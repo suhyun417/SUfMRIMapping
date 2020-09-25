fa845b03db46985b2d68b9cf738bd6bc56033deb
suhyun417 <soohyun.park@nih.gov>
2020-09-25 16:49:54 -0400

    rename the file

--- genFig_multipleFP_fig4.m
+++ genFig_multipleFP_fig4.m
@@ -1,4 +1,4 @@
-% genFig_multipleFP_fig4B.m
+% genFig_multipleFP_fig4.m
 %
 % 2020/09/11 SHP
 % Multiple face patch partially overlapping population
@@ -26,8 +26,8 @@ end
 
 setArea_order = [4 1 2 3]; % ML-AF-AM-AAM
 
-fig4B = figure;
-set(fig4B, 'Color', 'w', 'PaperPositionMode', 'auto');
+fig4A = figure;
+set(fig4A, 'Color', 'w', 'PaperPositionMode', 'auto');
 for iArea = 1:4
     idArea = setArea_order(iArea);
     sp(iArea) = subplot(4, 1, iArea);
@@ -40,3 +40,27 @@ end
 set(sp(:), 'Box', 'off', 'TickDir', 'out')
 set(sp(:), 'XColor', 'none')
 axis(sp(:), 'tight')
+
+
+aa = bsxfun(@minus, Clustering_meanROI.matR, mean(Clustering_meanROI.matR));
+[coeff, score, latent] = pca(aa);
+
+fig4B = figure;
+set(fig4B, 'Color', 'w', 'PaperPositionMode', 'auto');
+for iB = 1:3
+sp(iB) = subplot(1,3,iB);
+b(iB) = barh(coeff(orderROI, iB));
+b(iB).FaceColor = 'flat';
+    b(iB).CData = matRGB(orderROI, :);
+    b(iB).BarWidth = 1;
+    b(iB).EdgeColor = 'none';
+    title(sp(iB), sprintf('PC %d (%2.2f%%)', iB, latent(iB)*100))
+end
+set(sp(:), 'XLim', [-0.4 0.4])
+set(sp(:), 'YDir', 'reverse')
+set(sp(1), 'YTick', 1:37, 'YTickLabel', Clustering_meanROI.nameROI(orderROI))
+set(sp(2:3), 'YTick', 1:37, 'YTickLabel', [])
+set(sp(:), 'TickDir', 'out', 'Box', 'off')
+
+% set(gcf, 'Color', 'w', 'PaperPositionMode', 'auto')
+% set(b(:), 'FaceColor', 'flat', 'EdgeColor', 'none', 'CData', matRGB)
